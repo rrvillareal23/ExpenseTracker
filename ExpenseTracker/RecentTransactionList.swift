2123248b1e90e7994a4bb0912fc04ebb16c93199
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-19 12:00:22 -0700

    Added Recent Transaction view to show only 5 of recent transactions. Might amount later.

--- ExpenseTracker/RecentTransactionList.swift
+++ ExpenseTracker/RecentTransactionList.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct RecentTransactionList: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                // MARK: Header Title
+                Text("Recent Transactions")
+                    .bold()
+                
+                Spacer()
+                
+                // MARK: Hader Link
+                NavigationLink {
+                    EmptyView()
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("See All")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(Color.Text)
+                }
+            }
+            .padding(.top)
+            
+            // MARK: Recent Tranaction List
+            ForEach(Array(transactionListVM.transactions.prefix(5).enumerated()), id: \.element) {index, transaction in
+                TransactionRow(transaction: transaction)
+                
+                Divider()
+                    .opacity(index == 4 ? 0 : 1)
+            }
+        }
+        .padding()
+        .background(Color.systemBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.primary.opacity(0.2), radius: 10, x:0, y:5)
     }
 }
 
+let transactionListVM: TransactionListViewModel = {
+    let transactionListVM = TransactionListViewModel()
+    transactionListVM.transactions = transactionListPreviewData
+    return transactionListVM
+}()
+
 #Preview {
     RecentTransactionList()
+        .environmentObject(transactionListVM)
 }
