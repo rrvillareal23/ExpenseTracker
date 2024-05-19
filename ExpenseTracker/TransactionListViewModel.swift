42f82e634a24c27cc6a25db3aa484edbba24619a
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-19 10:47:15 -0700

    Created TransactionLIstViewModel to get dummy tranaction data from API

---
+++ ExpenseTracker/TransactionListViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  TransactionListViewModel.swift
+//  ExpenseTracker
+//
+//  Created by Ricky Villareal on 5/19/24.
+//
+
+import Foundation
+import Combine
+
+final class TransactionListViewModel: ObservableObject {
+    @Published var transactions: [Transaction] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        getTransactions()
+    }
+    
+    func getTransactions() {
+        guard let url = URL(string: "https://designcode.io/data/transactions.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) -> Data in
+                guard let httpReponse = response as? HTTPURLResponse, httpReponse.statusCode == 200 else {
+                    dump(response)
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: [Transaction].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .failure(let error):
+                    print("Error fetching transactions", error.localizedDescription)
+                case .finished:
+                    print("Finished fectching transaction")
+                }
+            } receiveValue: { [weak self] result in
+                self?.transactions = result
+            }
+            .store(in: &cancellables)
+    }
+}
