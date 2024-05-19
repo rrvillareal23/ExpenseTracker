215fd3f3ebd96fc7b148c0f25311ff54314b49e5
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-05-19 11:59:25 -0700

    Added more to the Model so it has category and subcategory logic. Also added code so categories get different icons.

---
+++ ExpenseTracker/RecentTransactionList.swift
@@ -0,0 +1,18 @@
+//
+//  RecentTransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Ricky Villareal on 5/19/24.
+//
+
+import SwiftUI
+
+struct RecentTransactionList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecentTransactionList()
+}
